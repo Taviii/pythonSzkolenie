ea4dc207841bbd53a3d882d7383eb118ceaeade0
Taviii <s21354@pjwstk.edu.pl>
2023-06-19 10:56:38 +0200

    first task

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-x = 1
-print (x)
+# x = 1
+# print (x)
+
+# for i in range(1, 12):
+#     print("Wartość iteratora to" + str(i))
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(10, 0, -1):
+#     print(i)
+#
+# for i in range(1, 20, 2):
+#     print(i)
+
+# lista = ["a", "b", "c", "d"]
+# for element in lista:
+#     print(element)
+#
+# print("________________")
+#
+# N = len(lista)
+# for i in range(N):
+#     print(lista[i])
+
+# i = 0
+# while (i < 11):
+#     print(i)
+#     i = i + 1
+
+# Zad 1 Wypisz liczby od 0 do 10 które są podzielne przez 6
+# for i in range(0, 101):
+#     if i % 3 == 0:
+#         if i % 2 == 0:
+#             print(i)
+
+for i in range(0, 101, 6):
+    print(i)
