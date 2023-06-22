8d4fc0a3c3eb3c274f1bf7191d85199272efa710
Taviii <s21354@pjwstk.edu.pl>
2023-06-22 14:06:33 +0200

    updated zadanieMacierz

--- zadanieMacierz.py
+++ zadanieMacierz.py
@@ -21,3 +21,33 @@ for row in matrix:
         matrix_sum += num
 
 print(matrix_sum)
+
+print("-----------------------------------")
+
+maxi =  list(map(max, matrix))
+print(maxi)
+
+print("-----------------------------------")
+#średnia arytm
+
+
+# def mean(row):
+#     matrix = [[row * 3 + col + 1 for col in range(3)] for row in range(3)]
+#     suma = [sum(row) for row in matrix]
+#     meanall = [s / 3 for s in suma]
+#     return meanall
+#
+#
+# meanall = list(map(mean, matrix))
+# print(meanall)
+
+lista_mean = list(map(lambda x: sum(x)/len(x), matrix))
+print(lista_mean)
+
+print("-----------------------")
+
+total_sum = sum(map(sum, matrix))
+total_count = sum(len(row) for row in matrix)
+
+matrix_mean = total_sum / total_count
+print(matrix_mean)
